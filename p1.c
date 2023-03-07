f126f3700a6bb3d0bc5eb738b9c9ad7bcad09b16
Lorudarku <47794989+Lorudarku@users.noreply.github.com>
2023-03-07 15:29:02 +0100

    Implementacion provisional acercarseLuz()

--- p1.c
+++ p1.c
@@ -1,28 +1,48 @@
 #pragma config(StandardModel, "EV3_REMBOT")
 
 ///// VARIABLES GLOBALES
-int sensorMinDistance = 10; //Distancia minima del sensor de ultrasonidos
-int robotSpeed = 20; //Velocdidad del robot
+int distUmbral = 50; //Distancia minima del sensor de ultrasonidos
+int robotSpeed = 90; //Velocdidad del robot
 
 /*
-Se acerca a una pared y cuando está cerca se para
+Se acerca a una pared y cuando está cerca reduce la velcidad de forma progresiva
 */
 task acercarsePared(){
 		setLEDColor(ledGreenFlash);
 		while(true){
-			if (getUSDistance(sonarSensor) > sensorMinDistance) {
-				if (getUSDistance(sonarSensor) <= 2*sensorMinDistance){
+			int d = getUSDistance(sonarSensor);
+			if (d > distUmbral) {
+				if (getUSDistance(sonarSensor) <= 2*distUmbral){
 					setLEDColor(ledOrangeFlash);
 				} else {
 				setLEDColor(ledGreenFlash);
 			}
 				setMotorSpeed(leftMotor, robotSpeed);
 				setMotorSpeed(rightMotor, robotSpeed);
-
 			}	else {
-				stopAllMotors();
+				setMotorSpeed(leftMotor, d);
+				setMotorSpeed(rightMotor, d);
 			}
-			abortTimeslice();
+	}
+}
+
+task acercarseLuz(){
+	int luzUmbral, luz;
+	luzUmbral = getColorAmbient(colorSensor)*1.4;
+	while(true){
+		luz = getColorAmbient(colorSensor);
+		if(luz > luzUmbral){
+			setMotorSpeed(leftMotor, 30);
+			setMotorSpeed(rightMotor, 0;
+			while (luz <= getColorAmbient(colorSensor)){
+				luz = getColorAmbient(colorSensor);
+			}
+			setMotorSpeed(leftMotor, 0);
+			setMotorSpeed(rightMotor, 30);
+			while (luz <= getColorAmbient(colorSensor)){
+				luz = getColorAmbient(colorSensor);
+			}
+		}
 	}
 }
 
@@ -38,8 +58,8 @@ task girarIzquierda(){
 					stopAllMotors();
 				} else {
 					int gradosDentro = getGyroDegrees(gyroSensor);
-					setMotorSpeed(leftMotor, -robotSpeed);
-					setMotorSpeed(rightMotor, robotSpeed);
+					setMotorSpeed(leftMotor, 30);
+					setMotorSpeed(rightMotor, 0;
 				}
 			}
 		abortTimeslice();
@@ -50,7 +70,8 @@ task girarIzquierda(){
 
 task main()
 {
-	startTask(acercarsePared);
+	//startTask(acercarsePared);
+	startTask(acercarseLuz);
 	//startTask(girarIzquierda);
 
 	while(true){
